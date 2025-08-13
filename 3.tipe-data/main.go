a513dcbd6468d21e76029515c60f2c4786393631
marlinps <marlinpurnamas92@gmail.com>
2025-08-13 15:06:07 +0700

    update 3.tipe-data

--- 3.tipe-data/main.go
+++ 3.tipe-data/main.go
@@ -24,6 +24,27 @@ func main() {
 	// TODO: Tipe Data string
 	var message string = "Halo"
 	fmt.Printf("message: %s \n", message)
+
+	/* TODO: Nilai nil & Zero Value
+
+	nil bukan merupakan tipe data, melainkan sebuah nilai. Variabel yg isi nilainya nil berarti memiiki nilai kosong
+
+	Semua tipe data yang sudah dibahas di atas memiliki zero value (nilai default tipe data). Artinya meskipun variabel dideklarasikan
+	dengan tanpa nilai awal, tetap akan ada nilai default-nya.
+
+	Zero value dari  string  adalah  ""  (string kosong).
+	Zero value dari  bool  adalah  false .
+	Zero value dari tipe numerik non-desimal adalah  0 .
+	Zero value dari tipe numerik desimal adalah  0.0 .
+
+	Zero value berbeda dengan nil. Nil adl nilai kosong, benar-benar kosong. Ada beberapa tipe data yang bisa di-set nilainya dengan  nil , di antaranya:
+	pointer
+	tipe data fungsi
+	slice
+	map
+	channel
+	interface kosong atau  interface{}
+	*/
 }
 
 /*  TODO: Tipe Data
