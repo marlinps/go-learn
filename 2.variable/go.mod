3b0282f5dbdc0ce3e02c561adc79efdd933cad2a
marlinps <marlinpurnamas92@gmail.com>
2025-08-13 10:56:07 +0700

    create variable

---
+++ 2.variable/go.mod
@@ -0,0 +1,3 @@
+module 2.variable
+
+go 1.22.5
---
+++ 2.variable/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// TODO: Deklarasi berserta Tipe Data
+	var firstName string = "john"
+
+	var lastName string
+	lastName = "wick"
+
+	fmt.Printf("Halo %s %s!\n", firstName, lastName)
+
+}
