667752858593ffda85aad3deb729765704aa2316
marlinps <marlinpurnamas92@gmail.com>
2025-08-14 09:59:24 +0700

    update  4.looping

--- 4.looping/main.go
+++ 4.looping/main.go
@@ -3,7 +3,15 @@ package main
 import "fmt"
 
 func main() {
+	// TODO: for
 	for i := 0; i < 5; i++ {
 		fmt.Println("Angka:", i)
+	}
+
+	// TODO: for = while (dengan argumen hanya kondisi)
+	j := 0
+	for j < 5 {
+		println("Angka:", j)
+		j++
 	}
 }
