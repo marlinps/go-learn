47c19fe84702e9cd52ba0d95b79bbf6cef532727
marlinps <marlinpurnamas92@gmail.com>
2025-08-14 14:55:11 +0700

    create 6.array

---
+++ 6.slice/go.mod
@@ -0,0 +1,3 @@
+module 5.slice
+
+go 1.22.5
---
+++ 6.slice/main.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+	// TODO: inisialisasi slice
+	slice := []int{1, 2, 3, 4, 5}   // slice
+	array := [5]int{6, 7, 8, 9, 10} //array
+	array2 := [3]int{11, 12, 13}    //array
+}
+
+/* TODO: notes
+Slice adl reference elemen array.
+perbedaan antara array dan slice:
+1. Array adl kumpulan nilai atau elemen, Array jumlah elemen didefinisikan
+2. Slice adl referensi tiap elemen, jumlah elemen tidak didefinisikan
+
+penjelasan detaill hal 52
+*/
