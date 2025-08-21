641c8cad8be890358a53832808acfe09a1b01ee2
Marlin Purnama Sari <marlinpurnamas92@gmail.com>
2025-08-21 13:52:47 +0700

    update slice

--- 6.slice/main.go
+++ 6.slice/main.go
@@ -5,8 +5,8 @@ import "fmt"
 func main() {
 	// TODO: inisialisasi slice
 	slice := []int{1, 2, 3, 4, 5}   // slice
-	array := [5]int{6, 7, 8, 9, 10} //array
-	array2 := [3]int{11, 12, 13}    //array
+	array := [5]int{6, 7, 8, 9, 10} // array
+	array2 := [3]int{11, 12, 13}    // array
 
 	_, _, _ = slice, array, array2
 
@@ -23,7 +23,7 @@ func main() {
 }
 
 /* TODO: notes
-Slice adl reference elemen array (Tipe Data Reference).
+Slice adl reference elemen array (Tipe Data Reference)
 perbedaan antara array dan slice:
 1. Array adl kumpulan nilai atau elemen, Array jumlah elemen didefinisikan
 2. Slice adl referensi tiap elemen, jumlah elemen tidak didefinisikan
