e53941bd2bf214d22abe42235545c5a5291a3f2b
marlinps <marlinpurnamas92@gmail.com>
2025-08-28 15:59:17 +0700

    create methods-pointers.go

---
+++ methods-pointers.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+type Rectangle struct {
+	Length float32
+	Width  float32
+}
+
+func (r Rectangle) Area() float32 {
+	return r.Length * r.Width
+}
+
+// TODO: delete * untuk melihat perbedaan method pointers dengan value receivers
+func (r *Rectangle) Scale(s float32) {
+	r.Length *= s
+	r.Width *= s
+}
+
+func main() {
+	rect := Rectangle{10, 5}
+	rect.Scale(2)
+	area := rect.Area()
+
+	fmt.Println("Area of Rectangle:", area)
+}
