4a4c07cba317f0e852621c5025fdb259c502afc3
marlinps <marlinpurnamas92@gmail.com>
2025-08-28 16:19:51 +0700

    update methods-pointers.go (add pointer receiver vs parameter poiter

--- methods-pointers.go
+++ methods-pointers.go
@@ -17,9 +17,20 @@ func (r *Rectangle) Scale(s float32) {
 	r.Width *= s
 }
 
+func Scale(r *Rectangle, s float32) {
+	r.Length *= s
+	r.Width *= s
+}
+
 func main() {
 	rect := Rectangle{10, 5}
+
+	// TODO: method with pointer receiver
 	rect.Scale(2)
+	area := rect.Area()
+
+	// TODO: function with parameter pointer
+	Scale(&rect, 2)
 	area := rect.Area()
 
 	fmt.Println("Area of Rectangle:", area)
