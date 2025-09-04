adf9e61f6832b538012c53951545a7f768b11ee6
Marlin Purnama Sari <marlinpurnamas92@gmail.com>
2025-09-04 10:39:09 +0700

    create 8.function (variadic function)

---
+++ 8.function/variadic-functions.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func Sum(numbers ...int) (total int, avg float64) {
+	total = 0
+	for _, number := range numbers {
+		total += number
+	}
+
+	avg = float64(total) / float64(len(numbers))
+	return
+
+}
+
+func main() {
+	total, avg := Sum(1, 2, 3, 4, 5)
+	fmt.Println("Total:", total)
+	fmt.Println("Average:", avg)
+
+}
