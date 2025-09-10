64b1ccbb2540a709fb642d4c7fad3d7279bfbe5d
marlinps <marlinpurnamas92@gmail.com>
2025-09-10 14:00:25 +0700

    update struct

--- 10.struct/main.go
+++ 10.struct/main.go
@@ -19,6 +19,19 @@ func main() {
 
 	fmt.Println("Student A:", studentA)
 	fmt.Println("Student B:", studentB)
+
+	// TODO: struct variable object pointer
+	var s2 *Student = &studentA                                 // pointer to variable pointer object
+	fmt.Println("Alamat memory Student A (using pointer):", s2) // print alamat memory
+	fmt.Println("Student A (value using pointer):", *s2)        // print value using pointer
+	fmt.Println("Student A Name (using pointer):", s2.Name)     // print value using pointer, llangsung akses field tanpa dereference *s2.Name
+
+	// TODO: keisitimewaan property dalam object pointer tanpa perlu deference nilai aslinya tetapi langsung akses fieldnya
+	s2.Name = "Fadhil"
+	s2.Grade = 9
+
+	fmt.Println("Student A (after modification using pointer):", studentA) // print value asli yang sudah di modifikasi menggunakan pointer
+
 }
 
 /* TODO: struct
