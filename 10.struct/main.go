f6a489f6ff15aa625df3aed57e216b5123612941
marlinps <marlinpurnamas92@gmail.com>
2025-09-10 13:20:37 +0700

    update struct

---
+++ 10.struct/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+type Student struct {
+	Name  string
+	Grade int
+}
+
+func main() {
+	var studentA Student
+	studentA.Name = "Budi"
+	studentA.Grade = 10
+
+	studentB := Student{
+		Name:  "Siti",
+		Grade: 9,
+	}
+
+	fmt.Println("Student A:", studentA)
+	fmt.Println("Student B:", studentB)
+
+}
+
+/* TODO: struct
+- struct adalah tipe data yang digunakan untuk mengelompokkan beberapa variable menjadi satu kesatuan
+- struct bisa berisi variable dengan tipe data yang berbeda
+- struct bisa diakses menggunakan dot notation (.)
+*/
