e7742aba85ba4b4579fcf8e02c47bc0b18ab3648
Marlin Purnama Sari <marlinpurnamas92@gmail.com>
2025-09-11 21:34:33 +0700

    update level-akses

--- 12.level-akses/main.go
+++ 12.level-akses/main.go
@@ -1,15 +1,29 @@
 package main
 
-import "12.level-akses/library"
+import (
+	. "12.level-akses/library" // menambahkan titi (.) untuk mengimport semua struct dan function di package library tanpa menulis library.
+
+	//TODO: pemanfaatan alias import
+	f "fmt"
+)
 
 func main() {
-	library.SayHello("Marlin")
+	// library.SayHello("Marlin")
+	SayHello("Marlin")
 
-	// TODO: untuk penggunaan struct pada package lain harus dipastikan nama struct dan propertynya harus berbentuk Exported(CamelCase)
-	student := library.Student{
+	// TODO: untuk penggunaan struct pada package lain harus dipastikan nama struct dan propertynya harus berbentuk Exported(CamelCase) kalau hanya memenuhi salah satu maka akan error
+	// student := library.Student{
+	// 	Name:  "Eko",
+	// 	Grade: 10,
+	// }
+
+	student := Student{
 		Name:  "Eko",
 		Grade: 10,
 	}
 
-	library.SayHello(student.Name)
+	//library.SayHello(student.Name)
+	SayHello(student.Name)
+
+	f.Println("Pemanfaatan alias import, menulisakan alias di awal import package")
 }
