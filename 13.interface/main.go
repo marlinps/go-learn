7025a8ab5fd247521e984ff3fa86ce8674a91e93
marlinps <marlinpurnamas92@gmail.com>
2025-10-07 14:03:16 +0700

    update interface (embedded interface)

--- 13.interface/main.go
+++ 13.interface/main.go
@@ -5,11 +5,20 @@ import (
 	"math"
 )
 
-type hitung interface {
+type hitung2d interface {
 	luas() float64
 	keliling() float64
 }
 
+type hitung3d interface {
+	volume() float64
+}
+
+// TODO: embedded interface
+type hitung interface {
+	hitung2d
+	hitung3d
+}
 type lingkaran struct {
 	jariJari float64
 }
@@ -34,6 +43,22 @@ func (p *persegi) keliling() float64 {
 	return 4 * p.sisi
 }
 
+type kubus struct {
+	sisi float64
+}
+
+func (k *kubus) volume() float64 {
+	return math.Pow(k.sisi, 3)
+}
+
+func (k *kubus) luas() float64 {
+	return 6 * math.Pow(k.sisi, 2)
+}
+
+func (k *kubus) keliling() float64 {
+	return 12 * k.sisi
+}
+
 func main() {
 	// TODO: pemanggilan langsung
 	l := lingkaran{4} // TODO: Go otomatis kasih &, jadi bisa dipanggil walaupun keliling() pointer receiver.
@@ -45,7 +70,7 @@ func main() {
 	fmt.Println("Keliling Persegi :", p.keliling())
 
 	// TODO: pemanggilan via interface
-	var bangunDatar hitung // inisialisasi variable object bertipe interface
+	var bangunDatar hitung2d // inisialisasi variable object bertipe interface
 	fmt.Println("Implementasi variable object bertipe interface")
 	bangunDatar = &lingkaran{4} //TODO:Tapi saat masuk ke interface, Go tidak otomatis kasih &, karena method set dicek ketat.
 	fmt.Printf("Luas Bangun Datar Lingkaran: %.2f\n", bangunDatar.luas())
@@ -54,6 +79,13 @@ func main() {
 	bangunDatar = &persegi{4}
 	fmt.Printf("Luas Bangun Datar Persegi: %.2f\n", bangunDatar.luas())
 	fmt.Printf("Keliling Bangun Datar Persegi: %.2f\n", bangunDatar.keliling())
+
+	// TODO: embedded interface
+	var bangunRuang hitung = &kubus{4}
+	fmt.Println("Implementasi Embedded Interface")
+	fmt.Printf("Luas Bangun Ruang Kubus: %.2f\n", bangunRuang.luas())
+	fmt.Printf("Keliling Bangun Ruang Kubus: %.2f\n", bangunRuang.keliling())
+	fmt.Printf("Volume Bangun Ruang Kubus: %.2f\n", bangunRuang.volume())
 
 }
 
